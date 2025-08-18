b59b9e4026042d9ac04b0b92a31b53edadeb0cea
pizmak11 <scigala.przemyslaw@gmail.com>
2025-08-18 13:03:16 +0200

    hotfix v2

--- pages/home/apartment-templates.php
+++ pages/home/apartment-templates.php
@@ -10,7 +10,7 @@
         <div class="apartment-templates__content">
           <p class="apartment-templates__price">Cena od <b>000000 zł</b></p>
           <img src="imgs/plans/plan-01.jpg" alt="" class="apartment-templates__img">
-          <a href="#" class="apartment-templates__link btn btn--primary">Sprawdź</a>
+          <a href="/mieszkanie.php" class="apartment-templates__link btn btn--primary">Sprawdź</a>
         </div>
       </div>
       <div class="apartment-templates__item">
@@ -18,7 +18,7 @@
         <div class="apartment-templates__content">
           <p class="apartment-templates__price">Cena od <b>000000 zł</b></p>
           <img src="imgs/plans/plan-02.jpg" alt="" class="apartment-templates__img">
-          <a href="#" class="apartment-templates__link btn btn--primary">Sprawdź</a>
+          <a href="/mieszkanie.php" class="apartment-templates__link btn btn--primary">Sprawdź</a>
         </div>
       </div>
       <div class="apartment-templates__item">
@@ -26,7 +26,7 @@
         <div class="apartment-templates__content">
           <p class="apartment-templates__price">Cena od <b>000000 zł</b></p>
           <img src="imgs/plans/plan-03.jpg" alt="" class="apartment-templates__img">
-          <a href="#" class="apartment-templates__link btn btn--primary">Sprawdź</a>
+          <a href="/mieszkanie.php" class="apartment-templates__link btn btn--primary">Sprawdź</a>
         </div>
       </div>
 
